6d2118f2b9e72df087e9f19fbdba0d2a8de6cddf
vincent <vincent77.matias77@gmail.com>
2024-05-15 14:21:49 +0100

    cration class ennemi et Zombie

---
+++ src/main/java/com/example/dernierespoirsae/modele/Ennemi.java
@@ -0,0 +1,8 @@
+package com.example.dernierespoirsae.modele;
+
+public class Ennemi extends Acteur{
+
+    public Ennemi(int x, int y, String nom, Environnement environnement) {
+        super(x, y, nom, environnement);
+    }
+}
---
+++ src/main/java/com/example/dernierespoirsae/modele/Zombie.java
@@ -0,0 +1,7 @@
+package com.example.dernierespoirsae.modele;
+
+public class Zombie extends Ennemi{
+    public Zombie(int x, int y, String nom, Environnement environnement) {
+        super(x, y, nom, environnement);
+    }
+}
