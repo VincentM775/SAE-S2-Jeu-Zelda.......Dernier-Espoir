928513172c6fdb9af31a33c400fe672374888735
Ariles Fellah <ariles.fellah937@gmail.com>
2024-05-16 10:46:06 +0200

    Maj de la vie et de la puissance de l'Acteur

--- src/main/java/com/example/dernierespoirsae/modele/Joueur.java
+++ src/main/java/com/example/dernierespoirsae/modele/Joueur.java
@@ -2,8 +2,7 @@ package com.example.dernierespoirsae.modele;
 
 public class Joueur extends Acteur{
 
-    public Joueur(int x, int y, String nom, Environnement environnement, int vie, int nombreDeDegat, int longTuile, int largeTuile, int nbTuile) {
-        super(x, y, nom, environnement, vie, nombreDeDegat, longTuile, largeTuile, nbTuile);
+    public Joueur(int x, int y, String nom, Environnement environnement, int longTuile, int largeTuile, int nbTuile) {
+        super(x, y, nom, environnement, 20, 100, longTuile, largeTuile, nbTuile);
     }
-
 }
