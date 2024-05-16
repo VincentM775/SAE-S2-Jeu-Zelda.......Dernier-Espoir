9b72de9ab1069d1d1cf1d01452d558617d766439
Fyromi <ariles.fellah937@gmail.com>
2024-05-16 10:07:47 +0200

    Creation d'une class Joueur

---
+++ src/main/java/com/example/dernierespoirsae/modele/Joueur.java
@@ -0,0 +1,9 @@
+package com.example.dernierespoirsae.modele;
+
+public class Joueur extends Acteur{
+
+    public Joueur(int x, int y, String nom, Environnement environnement, int vie, int nombreDeDegat, int longTuile, int largeTuile, int nbTuile) {
+        super(x, y, nom, environnement, vie, nombreDeDegat, longTuile, largeTuile, nbTuile);
+    }
+
+}
