a5503c46a9eb4160efd357ec890cfbbdb997c3ad
vincent <vincent77.matias77@gmail.com>
2024-05-31 22:03:41 +0100

    réglage vitesse

--- src/main/java/com/example/dernierespoirsae/controleur/Controleur.java
+++ src/main/java/com/example/dernierespoirsae/controleur/Controleur.java
@@ -93,7 +93,7 @@ public class Controleur implements Initializable {
 
         KeyFrame kf = new KeyFrame(
             // on définit le FPS (nbre de frame par seconde)
-            Duration.seconds((0.035)),
+            Duration.seconds((0.055)),
             // on définit ce qui se passe à chaque frame
             // c'est un eventHandler d'ou le lambda
             (ev ->{
